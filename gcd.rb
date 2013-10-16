8e334945937e181dbc35dc1bc5b7f8b5e2f8a7b1
Daniel Herzog <alu0100699494@ull.edu.es>
2013-10-16 10:35:54 +0000

    Arreglado error del fichero gcd.rb y creada el cuerpo de la clase Fraccion (junto con 2 métodos definidos) en el fichero racional.rb

--- gcd.rb
+++ gcd.rb
@@ -1,6 +1,6 @@
 def gcd(u, v)
   u, v = u.abs, v.abs
-  while v == 0
+  while v != 0 # ERROR, es !=, no ==
     u, v = v, u % v
   end
   u
--- racional.rb
+++ racional.rb
@@ -3,4 +3,47 @@
 require "./gcd.rb"
 
 class Fraccion
+	def initialize(numerador, denominador)
+		# Comprobar si son enteros
+		@numerador = numerador
+		if denominador < 0
+			# lanzar excepción, o corregir
+		end
+		@denominador = denominador
+		
+		# Reducir a expresión mínima
+		comun_div = gcd(numerador, denominador)
+		@numerador = @numerador/comun_div
+		@denominador = @denominador/comun_div
+	end
+	
+	def to_s
+		"#{@numerador}/#{@denominador}"
+	end
+	
+	# Nota: se puede hacer a/b + c ?
+	def suma(racional)
+		resultado
+		
+		resultado
+	end
+	
+	def resta(racional)
+		resultado
+		
+		resultado
+	end
+	
+	def producto(racional)
+		resultado
+		
+		resultado
+	end
+	
+	def division(racional)
+		resultado
+		
+		resultado
+	end
+	
 end
